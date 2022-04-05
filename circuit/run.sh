f194c3aa908ec849c6a11a1d880426c68bbe06ac
eigmax <stephen@ieigen.com>
2022-04-05 10:31:09 +0800

    use babyjubjub instead of p256k1

--- circuit/run.sh
+++ circuit/run.sh
@@ -14,22 +14,19 @@ node ../../scripts/generate_${circuit_name}.js
 #Prapare phase 1
 node generate_witness.js ${circuit_name}.wasm input.json witness.wtns
 
-
 #snarkjs powersoftau new bn128 16 pot16_0000.ptau -v
-snarkjs powersoftau new bn128 16 pot16_0000.ptau -v
+#
+#snarkjs powersoftau contribute pot16_0000.ptau pot16_0001.ptau --name="First contribution" -v
+#
+##Prapare phase 2
+#snarkjs powersoftau prepare phase2 pot16_0001.ptau pot16_final.ptau -v
 
-snarkjs powersoftau contribute pot16_0000.ptau pot16_0001.ptau --name="First contribution" -v
-
-#Prapare phase 2
-snarkjs powersoftau prepare phase2 pot16_0001.ptau pot16_final.ptau -v
-
-#snarkjs plonk setup ${circuit_name}.r1cs ~/Downloads/powersOfTau28_hez_final_16.ptau circuit_final.zkey
-snarkjs plonk setup ${circuit_name}.r1cs pot16_final.ptau circuit_final.zkey
+snarkjs groth16 setup ${circuit_name}.r1cs ~/Downloads/powersOfTau28_hez_final_16.ptau circuit_final.zkey
+#snarkjs plonk setup ${circuit_name}.r1cs pot16_final.ptau circuit_final.zkey
 snarkjs zkey export verificationkey circuit_final.zkey verification_key.json
-snarkjs plonk prove circuit_final.zkey witness.wtns proof.json public.json
-snarkjs plonk verify verification_key.json public.json proof.json
+snarkjs groth16 prove circuit_final.zkey witness.wtns proof.json public.json
+snarkjs groth16 verify verification_key.json public.json proof.json
 snarkjs zkey export soliditycalldata public.json proof.json
-cp public.json proof.json ../
 
 cd ..
 snarkjs zkey export solidityverifier ${base_dir}/circuit_final.zkey ../contracts/${circuit_name}_verifier.sol
--- test/pedersen_bjj.test.ts
+++ test/pedersen_bjj.test.ts
@@ -8,14 +8,29 @@ import {expect} from "chai";
 
 const buildBabyjub = require("circomlibjs").buildBabyjub;
 
+import { ContractFactory, BigNumberish } from "ethers";
+interface Proof {
+    a: [BigNumberish, BigNumberish];
+    b: [[BigNumberish, BigNumberish], [BigNumberish, BigNumberish]];
+    c: [BigNumberish, BigNumberish];
+}
+
+function parseProof(proof: any): Proof {
+    return {
+        a: [proof.pi_a[0], proof.pi_a[1]],
+        b: [
+            [proof.pi_b[0][1], proof.pi_b[0][0]],
+            [proof.pi_b[1][1], proof.pi_b[1][0]],
+        ],
+        c: [proof.pi_c[0], proof.pi_c[1]],
+    };
+}
 
 describe("Double Pedersen test", function() {
-    let babyJub;
     let circuit;
     let zkey
     let contract
     before( async() => {
-        babyJub = await buildBabyjub();
         let wasm = path.join(__dirname, "../circuit/pedersen_bjj_js", "pedersen_bjj.wasm");
         zkey = path.join(__dirname, "../circuit/pedersen_bjj_js", "circuit_final.zkey");
         let vkeypath = path.join(__dirname, "../circuit/pedersen_bjj_js", "verification_key.json");
@@ -23,29 +38,29 @@ describe("Double Pedersen test", function() {
         const buffer = fs.readFileSync(wasm);
         circuit = await wc(buffer);
 
-        let F = await ethers.getContractFactory("PedersenComm");
-        contract = await F.deploy();
+        let factory = await ethers.getContractFactory("PedersenComm");
+        contract = await factory.deploy();
         await contract.deployed();
     });
 
     // refer: https://github.com/iden3/circomlib/blob/master/test/pedersen.js
     it("Should pedersen at zero", async () => {
-
         const input = {
-            "in": ["1", "0"]
+            "in": ["11111", "01212121212"]
         }
         const witnessBuffer = await circuit.calculateWTNSBin(
             input,
             0
         );
-        const { proof, publicSignals } = await snarkjs.plonk.prove(zkey, witnessBuffer);
-        console.log(publicSignals)
-        const res = await snarkjs.plonk.exportSolidityCallData(proof, "");
-        let result = res.substring(0, res.length - 3);
-        console.log(res)
+        const { proof, publicSignals } = await snarkjs.groth16.prove(zkey, witnessBuffer);
+        console.log(proof)
+        const res = await snarkjs.groth16.exportSolidityCallData(proof, "");
+        //let result = res.substring(0, res.length - 3);
+        //console.log(res)
 
+        const {a, b, c} = parseProof(proof);
         let ok = await contract.verifyPedersenComm(
-            result,
+            a, b, c,
             publicSignals
         )
         console.log(ok)
